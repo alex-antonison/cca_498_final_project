5d5864fa09d6ca631db13d880f4f17aa7e0cc075
alex.antonison <alex.antonison@gmail.com>
2018-03-03 21:38:14 -0600

    Adding external create table statements for analyzing with Athena

---
+++ create_table_queries/create_table_questions.sql
@@ -0,0 +1,21 @@
+-- Author: Alex Antonison
+-- Create Date: 2018-03-03
+-- Description: This creates an external table that points to my bucket in s3 with the quesitons.
+
+-- Change Log
+-- User|Date|Change
+-- Alex|2018-03-03|Created
+
+create external table questions (
+  id int,
+  owner_user_id int,
+  creation_date timestamp,
+  score int,
+  title string,
+  body string
+  )
+COMMENT 'Stack overflow questions'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+location 's3://alex-antonison-data-lake/questions/'
---
+++ create_table_queries/create_table_tags.sql
@@ -0,0 +1,17 @@
+-- Author: Alex Antonison
+-- Create Date: 2018-03-03
+-- Description: This creates an external table for the tags stack overflow table that points to my bucket in s3.
+
+-- Change Log
+-- User|Date|Change
+-- Alex|2018-03-03|Created
+
+create external table tags (
+  id int,
+  tag string
+  )
+COMMENT 'Tags associate with stack overflow questions'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+location 's3://alex-antonison-data-lake/tags/'
