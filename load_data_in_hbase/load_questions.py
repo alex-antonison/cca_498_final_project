dd0c468861e41f2a483e4dd497632e35f919110d
Abhisek Jana <adeveloperdiary@gmail.com>
2018-03-06 02:45:01 -0500

    updated instructions on HBase

--- load_data_in_hbase/load_questions.py
+++ load_data_in_hbase/load_questions.py
@@ -9,10 +9,10 @@ df = spark.read.format('csv').option('header', 'true').option('mode', 'DROPMALFO
 
 
 def remove_html_tags(text):
-    soup = BeautifulSoup(text)
+    soup = BeautifulSoup(text,'html5lib')
     return soup.getText()
 
 
 rdd = df.rdd.map(lambda line: (line[0], line[1], line[2], line[3], remove_html_tags(line[4]), remove_html_tags(line[5]), line[4], line[5]))
 
-rdd.take(2)
+rdd.collect()
