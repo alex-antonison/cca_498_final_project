e67e655a344ae3f61137c485b18370f430e9ff73
Abhisek Jana <adeveloperdiary@gmail.com>
2018-03-07 01:08:43 -0500

    Preprocess Script

---
+++ preprocessor/a_pre_processor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+
+def remove_lines(text):
+    try:
+        return text.rstrip(os.linesep).replace('\n', ' ')
+    except:
+        print(text)
+
+
+# Before Executing this please open and save the file in Sublime Text to enforce UTF-8 encoding
+data = pd.read_csv('raw_data/full/Answers.csv')
+data = data.fillna(0)
+
+data['Body'] = data['Body'].apply(remove_lines)
+
+data.to_csv('raw_data/full/Answers_New.csv', encoding='utf-8', index=False)
