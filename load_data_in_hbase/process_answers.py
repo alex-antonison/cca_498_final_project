0c05f2ea2de9cf0c582508e8eaf19c3a76a0bd33
alex.antonison <alex.antonison@gmail.com>
2018-04-24 20:56:20 -0500

    WIP

--- load_data_in_hbase/process_answers.py
+++ load_data_in_hbase/process_answers.py
@@ -116,8 +116,8 @@ rdd = df.rdd.map(lambda line: (line[0], line[1], line[2], line[3], line[4], line
 
 
 
-rdd.foreachPartition(bulk_insert_hbase)
+# rdd.foreachPartition(bulk_insert_hbase)
 
-# rdd.foreachPartition(batch_insert_graph)
+rdd.foreachPartition(batch_insert_graph)
 
 spark.stop()
